f805ea891b494ee6390a15c01f89b46004fc953d
Acrylic125 <acrylic125email@gmail.com>
2022-12-26 12:02:09 +0800

    wip

---
+++ python/longest-consecutive-sequence.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/longest-consecutive-sequence/
+from typing import List
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        # We will come back to this another time.
+        return 0
