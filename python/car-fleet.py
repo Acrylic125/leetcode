2027fa29f92a361c9aa46dd1ce10557587e47a62
Acrylic125 <acrylic125email@gmail.com>
2022-12-27 08:38:39 +0800

    wip

---
+++ python/car-fleet.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/car-fleet/
+from typing import List
+
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        fleets = {}
+        cars = zip(position, speed)
+        for car in cars:
+            if car[0] >= target:
+                continue
+            else:
+                car[0] += car[1]
+                if car[0] >= target:
+                    fleets[car[0]] = car[1] 
+        while True:
+            
